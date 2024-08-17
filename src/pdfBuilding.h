2ef2a4992be961c998dcb940f41d5e054fd281c7
Sean Bikkes <bixkitts@gmail.com>
2024-08-17 14:21:16 +0200

    added pdf building skeleton

---
+++ src/pdfBuilding.h
@@ -0,0 +1,7 @@
+#include <stdlib.h>
+
+/* Should dynamically allocate a buffer
+ * to hold the markdown in pdf form and
+ * return a pointer to it.
+ */
+char *build_pdf_from_md(const char *md, size_t size);
