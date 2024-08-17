b9d95cca6b18591899d9144e0f8647f19fc3595c
Sean Bikkes <bixkitts@gmail.com>
2024-08-17 14:10:59 +0200

    star

--- src/fileHandling.h
+++ src/fileHandling.h
@@ -18,7 +18,7 @@ void            *memory_map_file    (const char *filename,
                                      size_t *file_size);
 void             unmap_file         (void *file_content,
                                      size_t file_size);
-struct LineInfo* parse_file         (char *file_content,
+struct LineInfo *parse_file         (char *file_content,
                                      size_t file_size,
                                      size_t *num_lines);
 int              write_data_to_file (const char *filename,
