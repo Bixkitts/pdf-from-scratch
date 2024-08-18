7a254d77854da87855c5a4c0893015f6c34cb2e4
MichalWojcik0 <michal_wojcik@zohomail.eu>
2024-08-18 10:41:19 +0200

    moved all the yappin to C comments so I can see whats actually going on

--- src/pdf_building.c
+++ src/pdf_building.c
@@ -7,99 +7,107 @@ const char *pdf_str_header  =
 "%PDF-2.0\nPOOP\n";
 
 const char *pdf_str_catalog =
-"1 0 obj %Catalog (root) object to locate everything else\n"
-"<</Type /Catalog\n"
-"/Pages 2 0 R\n"
-"/Metadata 7 0 R\n"
+"1 0 obj\n" //Catalog (root) object to locate everything else
+"<<"
+    "/Type /Catalog\n"
+    "/Pages 2 0 R\n"
+    "/Metadata 7 0 R\n"
 ">>\n"
 "endobj\n";
 
 const char *pdf_str_catalog_pages =
-"2 0 obj %Pages catalog, containing a single page\n"
-"<</Type /Pages\n"
-"/Kids [3 0 R]\n"
-"/Count 1\n"
+"2 0 obj\n" //Pages catalog, containing a single page
+"<<"
+    "/Type /Pages\n"
+    "/Kids [3 0 R]\n"
+    "/Count 1\n"
 ">>\n"
 "endobj\n";
 
 const char *pdf_str_pages =
-"3 0 obj %Page dictionary\n"
-"<</Type /Page\n"
-"/Parent 2 0 R\n"
-"/MediaBox [0 0 612 792]\n"
-"/Contents 4 0 R\n"
-"/Resources <</Font <</F1 5 0 R %Internal name, not font name\n"
-"/F2 6 0 R %Internal name, not font name\n"
-">>\n"
-">>\n"
+"3 0 obj\n" // %Page dictionary
+"<<"
+    "/Type /Page\n"
+    "/Parent 2 0 R\n"
+    "/MediaBox [0 0 612 792]\n"
+    "/Contents 4 0 R\n"
+    "/Resources <<"
+        "/Font <<"
+            "/F1 5 0 R\n" // %Internal ref, not font name
+            "/F2 6 0 R\n" // %Internal ref, not font name
+        ">>\n"
+    ">>\n"
 ">>\n"
 "endobj\n";
 
 const char *pdf_str_page_content =
-"4 0 obj %Page contents for page 1\n"
-"<</Length … number of bytes in page marking operators …\n"
+"4 0 obj\n" // %Page contents for page 1
+"<<"
+    "/Length … number of bytes in page marking operators …\n"
 ">>\n"
 "stream\n"
-"%This content stream writes \"Hello 32000-2 World\" and shows a number of\n"
-"%points of interest.\n"
-"%- There is no font styling in PDF, italic (or bold) is a change of font\n"
-"%- There is no automatic underlining in PDF, applications need to draw lines\n"
-"%- Applications will usually need to do font position calculations\n"
-"%- If extracting text, spaces are not always found in text strings, they\n"
-"%may come from changes to position.\n"
+        /*"%This content stream writes \"Hello 32000-2 World\" and shows a number of\n"
+        "%points of interest.\n"
+        "%- There is no font styling in PDF, italic (or bold) is a change of font\n"
+        "%- There is no automatic underlining in PDF, applications need to draw lines\n"
+        "%- Applications will usually need to do font position calculations\n"
+        "%- If extracting text, spaces are not always found in text strings, they\n"
+        "%may come from changes to position.\n"*/
 "BT\n"
-"/F1 24 Tf %Choose font F1, 24 points\n"
-"72 696 Td %Set initial x,y and set line start\n"
-"(Hello ) Tj %Show \"Hello \" including extra space\n"
-"/F2 24 Tf %Choose font F2, 24 points\n"
-"(32000-2) Tj %Show \"32000-2\"\n"
-"/F1 24 Tf %Choose font F1, 24 points\n"
-"156.1 0 Td %Position after implied space, relative to line\n"
-"start\n"
-"(Wor) Tj %Show \"Wor\" (implied space before is just\n"
-"visual)\n"
-"(ld) Tj %Finish word with \"ld\" (notice no space added)\n"
+    "/F1 24 Tf\n" // %Choose font F1, 24 points
+    "72 696 Td\n" // %Set initial x,y and set line start
+    "(Hello ) Tj\n" // %Show \"Hello \" including extra space
+    "/F2 24 Tf\n" // %Choose font F2, 24 points
+    "(32000-2) Tj\n" // %Show \"32000-2\"
+    "/F1 24 Tf\n" // %Choose font F1, 24 points
+    "156.1 0 Td\n" // %Position after implied space, relative to line
+    "start\n"
+    "(Wor) Tj\n" // %Show \"Wor\" (implied space before is just visual)
+    "(ld) Tj\n" // %Finish word with \"ld\" (notice no space added)
 "ET\n"
-"%Underline \"32000-2\". Position must be calculated by generator\n"
-"%including scaled per-font underline vertical offset\n"
-"133.3 694.2 m %Move to start of line position\n"
-"221.4 694.2 l %Set path to end of line position\n"
-"1.2 w %Set line width suitable for size of text\n"
-"S %Stroke path\n"
+/*"%Underline \"32000-2\". Position must be calculated by generator\n"
+"%including scaled per-font underline vertical offset\n"*/
+"133.3 694.2 m\n" // %Move to start of line position
+"221.4 694.2 l\n" // %Set path to end of line position
+"1.2 w\n" // %Set line width suitable for size of text
+"S\n" // %Stroke path
 "endstream\n"
 "endobj\n";
 
 const char *pdf_str_font_1 =
-"5 0 obj %Font dictionary (Helvetica)\n"
-"<</Type /Font\n"
-"/Subtype /Type1\n"
-"/BaseFont /Helvetica\n"
-"/Encoding /WinAnsiEncoding\n"
-"/FirstChar …\n"
-"/LastChar …\n"
-"/Widths […]\n"
-"/FontDescriptor …\n"
+"5 0 obj\n" // %Font dictionary (Helvetica)
+"<<"
+    "/Type /Font\n"
+    "/Subtype /Type1\n"
+    "/BaseFont /Helvetica\n"
+    "/Encoding /WinAnsiEncoding\n"
+    "/FirstChar …\n"
+    "/LastChar …\n"
+    "/Widths […]\n"
+    "/FontDescriptor …\n"
 ">>\n"
 "endobj\n";
 
 const char *pdf_str_font_2 =
-"6 0 obj %Font dictionary (Helvetica-Oblique)\n"
-"<</Type /Font\n"
-"/Subtype /Type1\n"
-"/BaseFont /Helvetica-Oblique\n"
-"/Encoding /WinAnsiEncoding\n"
-"/FirstChar …\n"
-"/LastChar …\n"
-"/Widths […]\n"
-"/FontDescriptor …\n"
+"6 0 obj\n" // %Font dictionary (Helvetica-Oblique)
+"<<"
+    "/Type /Font\n"
+    "/Subtype /Type1\n"
+    "/BaseFont /Helvetica-Oblique\n"
+    "/Encoding /WinAnsiEncoding\n"
+    "/FirstChar …\n"
+    "/LastChar …\n"
+    "/Widths […]\n"
+    "/FontDescriptor …\n"
 ">>\n"
 "endobj\n";
 
 const char *pdf_str_metadata =
-"7 0 obj %Document metadata\n"
-"<</Type /Metadata\n"
-"/Subtype /XML\n"
-"/Length … number of bytes in metadata …\n"
+"7 0 obj\n" // %Document metadata
+"<<"
+    "/Type /Metadata\n"
+    "/Subtype /XML\n"
+    "/Length … number of bytes in metadata …\n"
 ">>\n"
 "stream\n"
 "<?xpacket begin=\"… UTF-8 value of U+FEFF (efbbbf) …\" id=\"W5M0MpCehiHzreSzNTczkc9d\"?>\n"
