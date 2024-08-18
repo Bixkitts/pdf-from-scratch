bf1d4edb69a418f35527d6a4b515d7bccad1d87b
MichalWojcik0 <michal_wojcik@zohomail.eu>
2024-08-18 15:49:40 +0200

    str replace with unit test

--- src/main.c
+++ src/main.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
 
 #include "file_handling.h"
+#include "pdf_utils.h"
 
 int main(int argc, char *argv[]) {
     if (argc < 2) {
@@ -9,25 +11,36 @@ int main(int argc, char *argv[]) {
         exit(EXIT_FAILURE);
     }
 
-    const char *filename = argv[1];
+    const char *arg1 = argv[1];
+    if (strcmp(arg1, "test") == 0) {
+        do_tests();
+    }
+    else {
+        mmap_and_parse_file(arg1);
+    }
+    return 0;
+}
+
+static int mmap_and_parse_file(const char* filename) {
     size_t file_size;
-    char *file_content = memory_map_file(filename, &file_size);
+    char* file_content = memory_map_file(filename, &file_size);
     if (!file_content) {
         exit(EXIT_FAILURE);
     }
 
     size_t num_lines = 0;
-    struct LineInfo *lines = parse_file(file_content, file_size, &num_lines);
+    struct LineInfo* lines = parse_file(file_content, file_size, &num_lines);
 
     printf("Found %zu lines starting with '#':\n", num_lines);
     for (size_t i = 0; i < num_lines; i++) {
         printf("Line %zu: Start Index = %zu, Length = %zu\n",
-               i, lines[i].line_index, lines[i].line_length);
+            i, lines[i].line_index, lines[i].line_length);
     }
 
     free(lines);
     unmap_file(file_content, file_size);
-
-    return 0;
 }
 
+static int do_tests() {
+    assert(obj_replace_test() == 1);
+}
--- src/pdf_utils.h
+++ src/pdf_utils.h
@@ -1,4 +1,6 @@
 #include <string.h>
+#include <stdlib.h>
+#include <assert.h>
 
 size_t get_obj_length(const char *obj_strarr[], int len) {
 	size_t totalLength = 0;
@@ -8,11 +10,82 @@ size_t get_obj_length(const char *obj_strarr[], int len) {
 	return totalLength;
 }
 
-static const char* replace_left_delim = "{{{";
-static const char* replace_right_delim = "}}}";
+char *str_replace(char *orig, char *rep, char *with) {
+	char *result; 
+	char *ins;
+	char *tmp;
+	int len_rep;
+	int len_with;
+	int len_front;
+	int count;
 
-int replace_label_in_obj(const char *obj_strarr[], int len, const char *label, const char *replace) {
-	for (int i = 0; i < len; i++) {
+	if (!orig || !rep) return NULL;
+	len_rep = strlen(rep);
+	if (len_rep == 0) return NULL;
+	if (!with) with = strdup("");
+	len_with = strlen(with);
 
+	ins = orig;
+	for (count = 0; (tmp = strstr(ins, rep)); ++count) {
+		ins = tmp + len_rep;
 	}
+
+	tmp = result = (char*)malloc(strlen(orig) + (len_with - len_rep) * count + 1);
+
+	if (!result) return NULL;
+
+	while (count--) {
+		ins = strstr(orig, rep);
+		len_front = ins - orig;
+		tmp = strncpy(tmp, orig, len_front) + len_front;
+		tmp = strcpy(tmp, with) + len_with;
+		orig += len_front + len_rep;
+	}
+	strcpy(tmp, orig);
+	return result;
+}
+
+
+char* get_with_delim(char* str) {
+	static const char* replace_left_delim = "{{{";
+	static const size_t ldlen = sizeof("{{{") - 1;
+	static const char* replace_right_delim = "}}}";
+	static const size_t rdlen = sizeof("}}}") - 1;
+	size_t llen = strlen(str);
+	char* c_label = (char*)malloc(ldlen + rdlen + llen);
+	if(c_label == 0) return NULL;
+	strcpy(c_label, replace_left_delim);
+	strcpy(c_label + ldlen, str);
+	strcpy(c_label + ldlen + llen, replace_right_delim);
+
+	return c_label;
+}
+
+int replace_label_in_obj(char *obj_strarr[], int len, const char *label, const char *replace) {
+	char *c_label = get_with_delim(label);
+	if (c_label == 0) return 0;
+	char *out = NULL;
+	int cnt = 0;
+	
+	for (int i = 0; i < len; i++) {
+		if (out = str_replace(obj_strarr[i], c_label, strdup(replace))) {
+			obj_strarr[i] = out;
+			cnt++;
+		}
+	}
+	return cnt;
+}
+
+int obj_replace_test() {
+	char* ex_obj[] = {
+		"4 0 obj\n"
+			"<<"
+			"/Length {{{leee n}}}",
+			"\n>>\n"
+	"stream\n",
+	"{{{leee n}}}",
+	"endstream\n"
+	};
+
+	return replace_label_in_obj(ex_obj, 2, "leee n", "test") > 1;
 }
