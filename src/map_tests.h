5a074602d9904835b5ae7ea6609bb67e1b00bb3e
Sean Bikkes <bixkitts@gmail.com>
2024-08-23 23:07:56 +0200

    map tests

--- src/map_tests.h
+++ src/map_tests.h
@@ -5,26 +5,59 @@
 #include "test_util.h"
 #include "map.h"
 
-#define MAP_TEST_BIG_LEN 64
+#define MAP_TEST_BIG_LEN   64
+#define MAP_TEST_SMALL_LEN 16
 
 int test_map_big_insert(void)
 {
     struct map test_map = {}; 
+    const int  rubbish  = 111;
     new_map(&test_map);
-    char data   [MAP_TEST_BIG_LEN] = "test_data";
+
+    char data[MAP_TEST_BIG_LEN] = {};
+    memset(data, rubbish, MAP_TEST_BIG_LEN);
+
     char key_str[MAP_TEST_BIG_LEN] = "test_key";
     const struct map_key key = {.string = key_str,
                                 .len    = strlen(key_str)};
+
     map_cpy_insert(&test_map,
                    &key,
                    data,
                    MAP_TEST_BIG_LEN);
-    return(!strncmp(map_get(&test_map, &key)->data,
-                    data,
-                    64));
+    int res = (!strncmp(map_get(&test_map, &key)->data,
+                        data,
+                        MAP_TEST_BIG_LEN));
+    destroy_map(&test_map);
+    return res;
+}
+
+int test_map_small_insert(void)
+{
+    struct map test_map = {}; 
+    const int  rubbish  = 111;
+    new_map(&test_map);
+
+    char data[MAP_TEST_SMALL_LEN] = {};
+    memset(data, rubbish, MAP_TEST_SMALL_LEN);
+
+    char key_str[MAP_TEST_SMALL_LEN] = "test_key";
+    const struct map_key key = {.string = key_str,
+                                .len    = strlen(key_str)};
+
+    map_cpy_insert(&test_map,
+                   &key,
+                   data,
+                   MAP_TEST_SMALL_LEN);
+    int res = (!strncmp(map_get(&test_map, &key)->data,
+                        data,
+                        MAP_TEST_SMALL_LEN));
+    destroy_map(&test_map);
+    return res;
 }
 
 void do_map_tests(void)
 {
     RUNTEST(test_map_big_insert);
+    RUNTEST(test_map_small_insert);
 }
