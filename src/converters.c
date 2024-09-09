7131b654f3888c31d89b33aa3badf1a6664513e3
Sean Bix <bixkitts@gmail.com>
2024-09-09 13:43:56 +0200

    small note

--- src/converters.c
+++ src/converters.c
@@ -4,5 +4,7 @@ size_t convert_md_to_pdf(const char *in_data,
                          size_t in_data_len,
                          char **out_data)
 {
+    // Utilise pdf_utils and markdown_utils
+    // to do a conversion
     return 0;
 }
