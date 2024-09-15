c317c98ae154d92393254ffc2b69b499c9258d2a
Sean Bikkes <bixkitts@gmail.com>
2024-09-15 13:52:34 +0200

    started writing markdown parser

---
+++ src/markdown_utils.c
@@ -0,0 +1,13 @@
+#include "markdown_utils.h"
+
+int parse_markdown(const struct md_delimiter *delim,
+                   int delim_count,
+                   struct md_parse_result *out_result)
+{
+    return 0;
+}
+
+void delete_md_parse_result(struct md_parse_result *result)
+{
+
+}
--- src/markdown_utils.h
+++ src/markdown_utils.h
@@ -1,1 +1,22 @@
 #pragma once
+
+#include <stdio.h>
+
+#define MD_DELIMITER_LEN 16
+
+typedef size_t md_index_t;
+
+struct md_delimiter {
+    char open[MD_DELIMITER_LEN];
+    char close[MD_DELIMITER_LEN];
+};
+
+struct md_parse_result {
+    md_index_t *indices;
+    size_t *lengths;
+};
+
+int parse_markdown(const struct md_delimiter *delim,
+                   int delim_count,
+                   struct md_parse_result *out_result);
+void delete_md_parse_result(struct md_parse_result *result);
